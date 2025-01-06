1bbb04e743fb17bceeb5c0597050ff1cc3b75a9a
Alyaaa2 <nurraniam@gmail.com>
2025-01-06 19:59:55 +0700

    Update PingPong.py

---
+++ PingPong.py
@@ -0,0 +1,29 @@
+from pygame import *
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis_ball.png', 200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
